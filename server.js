01d9e4268d1e5c3d2a62c6085fa573b20b7475ca
mdsiaofficial <mdsiaofficial@gmail.com>
2025-02-08 13:34:28 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express();
+const path = require("path");
+
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req, res) => {
+  return res.render("index", { message: "First ejs message" });
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
+});
