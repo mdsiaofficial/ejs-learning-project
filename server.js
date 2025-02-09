d045cc5c2f5fb8deebe25a0a89b49ccf38338bdb
mdsiaofficial <mdsiaofficial@gmail.com>
2025-02-09 10:28:15 +0600

    Initialize project structure with basic files and configurations

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+console.log(__dirname)
+console.log(path.join(__dirname, "public"));
 
 
 app.get("/", (req, res) => {
